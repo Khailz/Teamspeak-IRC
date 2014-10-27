ab89198f4ed7445b22949d1b2ad1ce796a9d0cef
Khailz <bakermikhail@yahoo.com>
2014-10-26 21:50:44 -0400

    Start

---
+++ test.py
@@ -0,0 +1,8 @@
+__author__ = 'Khailz'
+
+__author__ = 'Khailz'
+
+user_nam = raw_input('username> ')
+filename = raw_input('> ')
+target = open(filename, 'w')
+target.write('user_name= %s' % user_nam)
