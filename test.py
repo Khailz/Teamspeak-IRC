2a950b3b62c67d8ebe52811d276d80e0692b9324
Khailz <bakermikhail@yahoo.com>
2014-10-27 16:47:36 -0400

    Start telenet

--- test.py
+++ test.py
@@ -2,9 +2,12 @@ __author__ = 'Khailz'
 
 import telnetlib
 
-HOST = "unlightedgaming.com"
+host = "unlightedgaming.com"
 port = 10011
 
-tn = telnetlib.Telnet(HOST)
+tn = telnetlib.Telnet(host, port, timeout=5)
+
+tn.read_until("login: ")
+tn.write("login serveradmin mikhail9")
 
 print tn.read_all()
