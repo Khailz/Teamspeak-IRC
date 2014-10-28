0ff7256ba948b565cd8e7f03d9e1eefc3da3f770
Khailz <bakermikhail@yahoo.com>
2014-10-27 23:47:50 -0400

    Added conf file with 3 variables. Neatened test.py to read from info.conf

--- test.py
+++ test.py
@@ -1,13 +1,32 @@
 __author__ = 'Khailz'
 
-import telnetlib
+import telnetlib, socket, time, ConfigParser, time
 
-host = "unlightedgaming.com"
-port = 10011
+#login Credidentials
+config = ConfigParser.ConfigParser()
+config.readfp(open(r'info.conf'))
+username = config.get('File', 'username')
+password = config.get('File', 'password')
+host = config.get('File', 'host')
+port = config.get('File', 'port')
 
-tn = telnetlib.Telnet(host, port, timeout=5)
 
-tn.read_until("login: ")
-tn.write("login serveradmin mikhail9")
+timeout = 30
 
-print tn.read_all()
+
+def connect_server(host, port):
+    try:
+        tn = telnetlib.Telnet(host, port, timeout)
+    except socket.timeout:
+        print ("socket timeout")
+    else:
+        tn.write("login %s %s\n" % (username, password))
+        tn.write("use 1\n")
+        tn.write("clientupdate client_nickname=Admin\n")
+        tn.write("clientpoke clid=2 msg=Connected\n")
+        tn.write("servernotifyregister event=textprivate\n")
+        tn.write("gm msg=Bot\sConnected!\n")
+        tn.read_until("notifytextmessage targetmode=1 msg=", timeout=9999)
+
+
+connect_server(host, port)
