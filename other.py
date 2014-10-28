e1b5ffcf93bf0ea90966a00c747c1435a1fcfbd2
Khailz <bakermikhail@yahoo.com>
2014-10-28 07:30:01 -0400

    Start connect from seperate sript

---
+++ other.py
@@ -0,0 +1,6 @@
+__author__ = 'Khailz'
+
+from test import connection
+
+start = connection.connect_server()
+connection.connect_server()
--- test.py
+++ test.py
@@ -2,31 +2,32 @@ __author__ = 'Khailz'
 
 import telnetlib, socket, time, ConfigParser, time
 
+class connection():
 #login Credidentials
-config = ConfigParser.ConfigParser()
-config.readfp(open(r'info.conf'))
-username = config.get('File', 'username')
-password = config.get('File', 'password')
-host = config.get('File', 'host')
-port = config.get('File', 'port')
+
+    config = ConfigParser.ConfigParser()
+    config.readfp(open(r'info.conf'))
+    username = config.get('File', 'username')
+    password = config.get('File', 'password')
+    host = config.get('File', 'host')
+    port = config.get('File', 'port')
+    timeout = 30
 
 
-timeout = 30
+    def connect_server(self, host, port):
+        try:
+            tn = telnetlib.Telnet(host, port, connection.timeout)
+        except socket.timeout:
+            print ("socket timeout")
+        else:
+            tn.write("login %s %s\n" % ("serveradmin", "mikhail9"))
+            tn.write("use 1\n")
+            tn.write("clientupdate client_nickname=Admin\n")
+            tn.write("clientpoke clid=2 msg=Connected\n")
+            tn.write("servernotifyregister event=textprivate\n")
+            tn.write("gm msg=Bot\sConnected!\n")
+            tn.read_until("notifytextmessage targetmode=1 msg=", timeout=9999)
 
 
-def connect_server(host, port):
-    try:
-        tn = telnetlib.Telnet(host, port, timeout)
-    except socket.timeout:
-        print ("socket timeout")
-    else:
-        tn.write("login %s %s\n" % (username, password))
-        tn.write("use 1\n")
-        tn.write("clientupdate client_nickname=Admin\n")
-        tn.write("clientpoke clid=2 msg=Connected\n")
-        tn.write("servernotifyregister event=textprivate\n")
-        tn.write("gm msg=Bot\sConnected!\n")
-        tn.read_until("notifytextmessage targetmode=1 msg=", timeout=9999)
+connection().connect_server("unlightedgaming.com", "10011")
 
-
-connect_server(host, port)
