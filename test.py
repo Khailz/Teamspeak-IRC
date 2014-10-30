21cf37803368eb8811e7faa0a3f3ce2334c35204
Khailz <bakermikhail@yahoo.com>
2014-10-30 06:45:39 -0400

    Moved config file to folder 'config' Added if info.cfg does not exist then copy info.cfg.sample Started working on getting user UID

--- test.py
+++ test.py
@@ -1,20 +1,38 @@
 __author__ = 'Khailz'
 
-import os, telnetlib, socket, time, ConfigParser, time
+import os, shutil, telnetlib, socket, ConfigParser, time
 
-os.chdir('C:/Users/311student/PycharmProjects/Teamspeak-IRC')
+config_file = os.path.realpath('config/info.cfg')
+example_file = os.path.realpath('config/info.cfg.sample')
+
+if not os.path.exists('config'):
+    os.makedirs('config')
+
+if os.path.isfile(config_file):
+    pass
+else:
+    shutil.copy(example_file, 'config')
+
+
+
 
 class connection():
-#login Credidentials
-
-    config_file = "info.cfg"
+    #login Credidentials
     config = ConfigParser.ConfigParser()
-    config.readfp(open(r'info.cfg'))
+    config.readfp(open(r"config/info.cfg"))
     username = config.get('CONFIG', 'username')
     password = config.get('CONFIG', 'password')
     host = config.get('CONFIG', 'host')
     port = config.get('CONFIG', 'port')
     timeout = 30
+
+    #def invalid_cfg(self):
+
+        #if config_file("username="):
+
+
+
+
 
 
     def connect_server(self, host, port):
@@ -23,18 +41,51 @@ class connection():
         except socket.timeout:
             print ("socket timeout")
         except socket.error:
-            print ("socket timeout")
+            print ("socket error")
         else:
-            tn.write("login %s %s\n" % ("serveradmin", "mikhail9"))
+            tn.write("login %s %s\n" % (connection.username, connection.password))
+            print "Succesfully Logged in!\n"
+            time.sleep(1)
             tn.write("use 1\n")
+            print "Using 1..\n"
+            time.sleep(1)
             tn.write("clientupdate client_nickname=Admin\n")
-            tn.write("clientpoke clid=2 msg=Connected\n")
+            print "Updating name...\n"
+            time.sleep(1)
+            tn.write("clientpoke clid=9 msg=Connected\n")
+            print "Testing poke...\n"
+            time.sleep(1)
             tn.write("servernotifyregister event=textprivate\n")
+            print "Accepting messages!\n"
+            time.sleep(1)
             tn.write("gm msg=Bot\sConnected!\n")
-            tn.read_until("notifytextmessage targetmode=1 msg=", timeout=9999)
+            print "Global Message for succesful bot connection sent!\n"
+            tn.write("%s" % commands.command1)
+            print tn.read_eager()
+
+            while True:
+                print tn.read_until("notifytextmessage targetmode=1 msg=")
+
+            #tn.read_all#("notifytextmessage targetmode=1 msg=", timeout=20)
+
+class commands():
+
+    command1 = ("gm msg=Hi\n")
+
+    def get_user_uuid(self):
+
+        """
+This will recieve user ID and UUID
+from teamspeak and convert it to a
+variable.
+
+        """
+        user
+
+
+connection().connect_server(connection.host, connection.port)
 
 
 
 
-connection().connect_server("unlightedgaming.com", "10011")
 
